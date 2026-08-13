34db93a3bce6c44b23b518de06d6ce505fc100b9
gad-biton <gadb633@gmail.com>
2026-08-13 15:10:58 +0300

    added ask_for_expense

--- Show_all_expenses.py
+++ Show_all_expenses.py
@@ -15,3 +15,9 @@ print(f"{calculate_total(list_of_expenses)} ILS")
 def add_expense(list_of_expenses,titel,category,amount):
     list_of_expenses.append({"date":strftime("%Y-%m-%d"),"titel": titel,"category":category,"amount":amount })
     return list_of_expenses
+def ask_for_expense(list_of_expenses):
+    titel = input("enter titel")
+    category = input("enter category")
+    amount = input("enter amount")
+    add_expense(list_of_expenses,titel,category,amount)
+    
