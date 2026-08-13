0fbe53e2838e30283e51aafe2b9ef9f8f8f9c110
gad-biton <gadb633@gmail.com>
2026-08-13 15:06:40 +0300

    added add expese

--- Show_all_expenses.py
+++ Show_all_expenses.py
@@ -14,4 +14,4 @@ print(f"{calculate_total(list_of_expenses)} ILS")
 
 def add_expense(list_of_expenses,titel,category,amount):
     list_of_expenses.append({"date":strftime("%Y-%m-%d"),"titel": titel,"category":category,"amount":amount })
-    
+    return list_of_expenses
