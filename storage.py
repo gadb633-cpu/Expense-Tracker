da950238b58203b55399742be9c9f6a3b2d183b4
gad-biton <gadb633@gmail.com>
2026-08-14 16:19:11 +0300

    fixed section 3

--- storage.py
+++ storage.py
@@ -4,10 +4,13 @@ app = Typer()
 
 @app.command()
 def add(title, category, amount):
+    amount = float(amount)
     add_expense(list_of_expenses, title, category, amount)
     show_expenses(list_of_expenses)
-
+    console = Console()
+    console.print(f"Total: [bold green]${calculate_total(list_of_expenses):.2f}[/bold green]")
 @app.command("list")
 def show_list():
     show_expenses(list_of_expenses)
-
+    console = Console()
+    console.print(f"Total: [bold green]${calculate_total(list_of_expenses):.2f}[/bold green]")
